040c55234e3552ff8f2a7bb9c03878e2e5431537
BugHunter-X <askzenai@gmail.com>
2025-01-09 13:42:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,8 @@
+The following improved code demonstrates a more robust approach to handling `NSDecimalNumber` calculations:
+
+```objectivec
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSDecimalNumber *num1 = [NSDecimalNumber decimalNumberWithString:@
